5f5d6986073ba0924081cfa1768b3a56efff31cb
Jacob Bisnett <jacob.bisnett@gmail.com>
2017-04-13 20:56:34 -0400

    formatting and change to a more benchable format

--- crate_pass/src/main.rs
+++ crate_pass/src/main.rs
@@ -1,40 +1,36 @@
 #![allow(dead_code)]
-#![feature(plugin, test, custom_derive)]
+#![feature(plugin, custom_derive)]
 #![plugin(compiler)]
 #[affinity_groups(a = 1, b = 2, c = 1)]
 #[derive(Debug)]
 #[derive(Clone)]
 struct Test {
-  pub a: usize,
-  pub b: usize,
-  pub c: usize,
+	pub a: usize,
+	pub b: usize,
+	pub c: usize,
 }
 
 impl Copy for Test {}
 
-#[cfg(test)]
-mod bench {
-  fn simple(b: &mut Bencher) {
-    let mut y = [Test { a: 0, b: 0, c: 0 }; 10000];
-  }
-
-}
-
 #[allow(unused_variables)]
 fn main() {
-  let mut y = [Test { a: 0, b: 0, c: 0 }; 10000];
-  for i in 0..10000 {
-    y[i].a = i;
-    y[i].b = i + 1;
-  }
-  for i in 0..10000 {
-    y[i].a += y[i].a;
-    y[i].c += y[i].c;
-  }
-  for i in 0..10000 {
-    y[i].b += y[i].b
-  }
-  for i in 0..10000 {
-    println!{"{:?} {:?} {:?}", y[i].a, y[i].b, y[i].c};
-  }
+	let mut y = [Test { a: 0, b: 0, c: 0 }; 10000];
+	for i in 0..10000 {
+		y[i].a = i;
+		y[i].b = i + 1;
+	}
+	for i in 0..10000 {
+		y[i].a += y[i].a;
+		y[i].c += y[i].c;
+	}
+	for i in 0..10000 {
+		y[i].b += y[i].b
+	}
+	let mut sum = 0;
+	for i in 0..10000 {
+		sum += y[i].a;
+		sum += y[i].b;
+		sum += y[i].c;
+	}
+	println!{"Total sum is: {}", sum};
 }
